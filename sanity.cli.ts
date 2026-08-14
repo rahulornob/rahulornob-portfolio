9ba488e3c61e6a24660d8c6b63165e47638af77a
rahulornob <rahulornob@users.noreply.github.com>
2026-08-15 02:05:51 +0600

    Fix hosted Sanity Studio configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,8 +4,8 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'eo19umac'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export default defineCliConfig({
   api: { projectId, dataset },
--- sanity.config.ts
+++ sanity.config.ts
@@ -8,10 +8,12 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schema} from './src/sanity/schemaTypes'
 import {structure} from './src/sanity/structure'
+
+const apiVersion = '2026-08-13'
+const dataset = 'production'
+const projectId = 'eo19umac'
 
 export default defineConfig({
   basePath: '/studio',
