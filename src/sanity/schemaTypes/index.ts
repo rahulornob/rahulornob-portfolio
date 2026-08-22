91399fcd742936a1b9e95e8f011a2732b421c8d6
rahulornob <rahulornob@users.noreply.github.com>
2026-08-23 02:13:28 +0600

    Remove obsolete Sanity source files

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import {siteSettings} from './siteSettings'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [siteSettings],
-}
